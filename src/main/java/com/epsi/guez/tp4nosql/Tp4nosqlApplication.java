288c18264c2bbc285fccd1c2ea2b9ca15e103be3
Jordan Noel <jordannoel1@live.fr>
2019-04-03 14:28:00 +0200

    Initial commit

---
+++ src/main/java/com/epsi/guez/tp4nosql/Tp4nosqlApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.guez.tp4nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4nosqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4nosqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epsi/guez/tp4nosql/Tp4nosqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epsi.guez.tp4nosql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tp4nosqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
